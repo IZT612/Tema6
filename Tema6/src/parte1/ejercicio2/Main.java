009204c8ad426151393956d7830fbd4c794f8915
IZT612 <ivan.zamora@iesnervion.es>
2025-04-22 14:36:07 +0200

    ejercicio 3 acabado

---
+++ Tema6/src/parte1/ejercicio2/Main.java
@@ -0,0 +1,37 @@
+package parte1.ejercicio2;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		double suma = 0;
+		double media = 0;
+		int cont = 0;
+		
+		try (Scanner sc = new Scanner(new FileReader("src\\parte1\\ejercicio2\\Enteros"))) {
+			
+			while (sc.hasNextDouble()) {
+				
+				suma += sc.nextDouble();
+				cont++;
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			
+			System.out.println(e.getLocalizedMessage());
+			
+		}
+		
+		media = suma / cont;
+		
+		System.out.println("Suma: " + suma);
+		System.out.println("Media: " + media);
+
+	}
+
+}
---
+++ Tema6/src/parte1/ejercicio3/Main.java
@@ -0,0 +1,54 @@
+package parte1.ejercicio3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		String linea;
+		int edad;
+		double estatura;
+		String nombre;
+		String todo[];
+		String nombres = "";
+		double edadMedia = 0;
+		double estaturaMedia = 0;
+		int cont = 0;
+		
+		try {
+			
+			BufferedReader in = new BufferedReader(new FileReader("src\\parte1\\ejercicio3\\Alumnos"));
+			
+			linea = in.readLine();
+			
+			while (linea != null) {
+				todo = linea.split(" ");
+				nombre = todo[0];
+				edad = Integer.parseInt(todo[1]);
+				estatura = Double.parseDouble(todo[2]);
+				
+				nombres += nombre + " ";
+				edadMedia += edad;
+				estaturaMedia += estatura;
+				
+				cont++;
+				
+				linea = in.readLine();			
+			} 
+			 
+			System.out.println("Nombres: " + nombres);
+			System.out.println("Edad media: " + (edadMedia / cont));
+			System.out.println("Estatura media: " + (estaturaMedia / cont));
+		
+		} catch (IOException e) {
+			
+			System.out.println(e.getLocalizedMessage());
+			
+		}
+		
+	}
+
+}
