d96dc5277832b8699e17ead53ac7af9c6689e040
IZT612 <ivan.zamora@iesnervion.es>
2025-04-25 14:37:39 +0200

    Ejercicio 7 acabado en condiciones

--- Tema6/src/parte1/ejercicio7/Main.java
+++ Tema6/src/parte1/ejercicio7/Main.java
@@ -2,249 +2,166 @@ package parte1.ejercicio7;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
 		
-		int opcion = 0;
+		BufferedWriter out = new BufferedWriter(new FileWriter("src\\parte1\\ejercicio7\\agenda.txt", true));
+
+		HashSet<String> agenda = new HashSet<String>();
+		
+		String linea;
+		
+		try (BufferedReader in = new BufferedReader(new FileReader("src\\parte1\\ejercicio7\\agenda.txt"))) {
+			
+			linea = in.readLine();			
+			
+			while (linea != null && !linea.equals("")) {
+				
+				agenda.add(linea);
+				linea = in.readLine();
+				
+			}
+			
+		} catch (FileNotFoundException e) {
+			
+			FileWriter f;
+			
+			try {
+				
+				f = new FileWriter("src\\parte1\\ejercicio7d\\agenda.txt");
+				f.close();
+				
+			} catch (IOException e1) {
+				
+				System.out.println(e.getLocalizedMessage());
+				
+			}
+			
+		}
+
+		int opcion;
 		String nombre;
 		int numero;
-		
+
 		do {
-			
+
 			System.out.println("Introduzca alguna de las siguientes opciones:");
 			System.out.println("1. Nuevo contacto.");
 			System.out.println("2. Buscar por nombre.");
 			System.out.println("3. Mostrar todos.");
-			System.out.println("4. Salir.");
+			System.out.println("4. Guardar y salir.");
 			opcion = sc.nextInt();
 			sc.nextLine();
 			System.out.println();
-			
-			switch(opcion) {
-			
+
+			switch (opcion) {
+
 			case 1 -> {
-				
-				System.out.println("Introduzca el nombre.");
+
+				String datos;
+				boolean encontrado = false;
+
+				System.out.println("Introduzca el nombre y el número respectivamente:");
 				nombre = sc.nextLine();
-				System.out.println("Ahora el número.");
 				numero = sc.nextInt();
-				System.out.println();
-				
-				if(nuevoContacto(nombre, numero)) {
+				sc.nextLine();
+
+				for (String contacto : agenda) {
+
+					if (contacto.substring(0, contacto.indexOf(' ')).equals(nombre)) {
+
+						encontrado = true;
+
+					}
+
+				}
+
+				if (!encontrado) {
+
+					datos = nombre + " " + numero;
+
+					agenda.add(datos);
+					out.append(datos);
+					out.newLine();
 					
-					System.out.println("Se ha creado el contacto correctamente.");
+				} else if (encontrado) {
 					
-				} else {
+					System.out.println("Este contacto ya existe.");
 					
-					System.out.println("La agenda está llena o ese contacto ya existe.");
+				} else if (agenda.size() == 20) {
+					
+					System.out.println("La agenda está llena.");
 					
 				}
 				
-				
 			}
-			
+
 			case 2 -> {
-				
-				System.out.println("Introduzca el nombre del contacto a buscar.");
-				nombre = sc.nextLine();
-				numero = buscarNombre(nombre);
-				
-				if (numero != -1) {
-					
-					System.out.println(nombre + ": " + numero);
-					
-				} else {
-					
-					System.out.println("No se ha encontrado ningún contacto con ese nombre.");
-					
+
+				String nombreBuscar;
+				String nombreContacto;
+				boolean encontrado = false;
+
+				System.out.println("Introduzca el nombre a buscar.");
+				nombreBuscar = sc.nextLine();
+
+				for (String contacto : agenda) {
+
+					nombreContacto = contacto.substring(0, contacto.indexOf(' '));
+
+					if (nombreBuscar.equals(nombreContacto)) {
+
+						System.out.println();
+						System.out.println(nombreContacto + ": " + contacto.substring(contacto.indexOf(' ') + 1));
+						encontrado = true;
+
+					}
+
 				}
-				
+
+				if (!encontrado) {
+
+					System.out.println();
+					System.out.println("No se ha encontrado el contacto.");
+
+				}
+
 			}
-			
+
 			case 3 -> {
-				
-				System.out.println(mostrarTodos());
-				
+
+				System.out.println();
+				for (String contacto : agenda) {
+
+					System.out.println(contacto.substring(0, contacto.indexOf(' ')) + ": "
+							+ contacto.substring(contacto.indexOf(' ') + 1));
+
+				}
+
 			}
-			
-			
+
 			}
-			
+
 			System.out.println();
-			
+
 		} while (opcion != 4);
-		
+
 		System.out.println("Saliendo del programa.");
-		
+
+		out.flush();
+		out.close();
+
 		sc.close();
 
-	}
-	
-	static public String mostrarTodos() {
-		
-		String contactos = "";
-		
-		String linea;
-		String[] datosLinea;
-		
-		try(BufferedReader out = new BufferedReader(new FileReader("src\\parte1\\ejercicio7\\agenda.txt"))) {
-			
-			linea = out.readLine();
-			while (linea != null && !linea.equals("")) {
-				
-				datosLinea = linea.split(" ");
-				contactos += datosLinea[0] + ": " + datosLinea[1] + "\n";
-				linea = out.readLine();
-				
-			}
-			
-		} catch (IOException e) {
-			
-			System.out.println(e.getLocalizedMessage());
-			
-		}
-		
-		// Para eliminar el ultimo "\n"
-		contactos = contactos.substring(0, (contactos.length() - 2));
-		
-		return contactos;
-		
-	}
-	
-	static public boolean nuevoContacto(String nombre, int numero) {
-		
-		boolean conseguido = true; 
-		
-		if (!estaLleno() && !nombreExistente(nombre)) {
-			
-			try(BufferedWriter in = new BufferedWriter(new FileWriter("src\\parte1\\ejercicio7\\agenda.txt", true))) {
-				
-				in.append(nombre + " " + numero);
-				in.newLine();
-				in.flush();
-				in.close();
-				
-			} catch (IOException e) {
-				
-				System.out.println(e.getLocalizedMessage());
-				conseguido = false;
-			}
-			
-		} else {
-			
-			conseguido = false;
-			
-		}
-		
-		return conseguido;
-		
-	}
-	
-	static public int buscarNombre(String nombre) {
-		
-		int numero = -1;
-		
-		String linea;
-		String[] datosLinea;
-		
-		try(BufferedReader out = new BufferedReader(new FileReader("src\\parte1\\ejercicio7\\agenda.txt"))) {
-			
-			linea = out.readLine();
-			
-			while(numero == -1 && linea != null && !linea.equals("")) {
-				
-				datosLinea = linea.split(" ");
-				if (datosLinea[0].equals(nombre)) {
-					
-					numero = Integer.parseInt(datosLinea[1]);
-					
-				}
-				linea = out.readLine();
-				
-			}
-			
-		} catch (IOException e) {
-			
-			System.out.println(e.getLocalizedMessage());
-			
-		}
-		
-		return numero;
-		
-	}
-	
-	static public boolean estaLleno() {
-		
-		boolean lleno = false;
-		
-		int contador = 0;
-		String linea;
-		
-		try(BufferedReader out = new BufferedReader(new FileReader("src\\parte1\\ejercicio7\\agenda.txt"))) {
-			
-			linea = out.readLine();
-			
-			while(linea != null && !linea.equals("")) {
-				
-				contador++;
-				linea = out.readLine();
-				
-			}
-			
-		} catch (IOException e) {
-			
-			System.out.println(e.getLocalizedMessage());
-			
-		}
-		
-		if (contador == 20) {
-			
-			lleno = true;
-			
-		}
-		
-		return lleno;
-		
-	}
-	
-	static public boolean nombreExistente(String nombre) {
-		
-		boolean encontrado = false;
-		
-		String linea;
-		String[] datosLinea;
-		
-		try(BufferedReader out = new BufferedReader(new FileReader("src\\parte1\\ejercicio7\\agenda.txt"))) {
-			
-			linea = out.readLine();
-			
-			while(!encontrado && linea != null && !linea.equals("")) {
-				
-				datosLinea = linea.split(" ");
-				if (datosLinea[0].equals(nombre)) {
-					
-					encontrado = true;
-					
-				}
-				linea = out.readLine();
-				
-			}
-			
-		} catch (IOException e) {
-			
-			System.out.println(e.getLocalizedMessage());
-			
-		}
-		
-		return encontrado;
-		
 	}
 
 }
